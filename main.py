96e7d83b9ebb0a62269997cba6a5d38bc608b3b9
Surender6 <gudasurendar6@gmail.com>
2024-12-23 18:21:44 +0530

    model training is in progress

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.cnnClassifier import logger
 
 from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline 
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.stage_03_training import ModelTrainingPipeline
 
 STAGE_NAME = "Data Igestion stage"
 try:
@@ -24,3 +25,14 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e 
+
+
+STAGE_NAME = "Training"
+try:
+  logger.info(f"**********")
+  logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+  model_traier = ModelTrainingPipeline()
+  model_traier.main()
+  logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\n\nX=========X")
+except Exception as e:
+        raise e
